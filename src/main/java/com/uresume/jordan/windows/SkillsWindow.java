2a236dca6aecf100645875107fb1bb1f8924291b
Javatlacati <scherzo_16@hotmail.com>
2019-10-12 18:03:41 -0500

    Modify files structure to match maven standard directory layout

---
+++ src/main/java/com/uresume/jordan/windows/SkillsWindow.java
@@ -0,0 +1,9 @@
+package com.uresume.jordan.windows;
+
+import javax.swing.JFrame;
+import javax.swing.JList;
+
+public class SkillsWindow {
+    public SkillsWindow(JFrame window, JList<String> skillsF) {
+    }
+}
